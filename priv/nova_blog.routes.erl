0fe9a499a95f8deea594afd7894732f681f31e35
Niclas Axelsson <niclas@burbas.se>
2019-08-09 14:59:43 +0200

    Add blog and fix some routes and includes

--- priv/nova_blog.routes.erl
+++ priv/nova_blog.routes.erl
@@ -2,9 +2,10 @@
   type => html,
   security => false,
   routes => [
-            {"/", nova_blog_main_controller, index}
-           ],
- statics => [
-             {"/assets/[...]", "assets"}
-            ]
-}.
+             {"/", nova_blog_main_controller, index},
+             {"/blog", nova_blog_blog_controller, index}
+            ],
+  statics => [
+              {"/assets/[...]", "assets"}
+             ]
+ }.

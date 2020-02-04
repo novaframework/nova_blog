bbd7ba55d960810aba2fdced4e1b80b77c403b3a
Niclas Axelsson <niclas@burbas.se>
2020-02-04 21:03:35 +0100

    Make the routes a bit nicer and include new page for documentation

--- priv/nova_blog.routes.erl
+++ priv/nova_blog.routes.erl
@@ -5,15 +5,12 @@
              {"/blog", {nova_blog_blog_controller, index}},
              {"/release/[:version]",{nova_blog_release_controller, index}, #{}},
              {"/article/[:id]",{nova_blog_article_controller,index},#{}},
+             {"/docs/[:page]", {nova_blog_documentation_controller, index}, #{}},
+             {"/community",{nova_blog_community_controller,index},#{}},
+             {"/forum",{nova_blog_forum_controller,index},#{}},
              {404, {nova_blog_main_controller, not_found}}
             ],
   statics => [
               {"/assets/[...]", "assets"}
              ]
  }.
-#{application => nova_blog,host => '_',prefix => [],
-  routes => [{"/community",{nova_blog_community_controller,index},#{}}],
-  security => false}.
-#{application => nova_blog,host => '_',prefix => [],
-  routes => [{"/forum",{nova_blog_forum_controller,index},#{}}],
-  security => false}.
